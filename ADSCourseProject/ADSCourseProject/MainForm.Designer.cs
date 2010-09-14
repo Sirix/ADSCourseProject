9c8d65d60ca5e4e02ab1600df6eeb084b0e0d9f0
Sirix <Sirix25@gmail.com>
2010-09-14 19:36:56 +0300

    First implementation of graph generator - bugged and not correctly working

--- ADSCourseProject/ADSCourseProject/MainForm.Designer.cs
+++ ADSCourseProject/ADSCourseProject/MainForm.Designer.cs
@@ -28,20 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.gViewer = new Microsoft.Glee.GraphViewerGdi.GViewer();
+            this.nComputersCount = new System.Windows.Forms.NumericUpDown();
+            this.lblComputersCount = new System.Windows.Forms.Label();
+            this.lblConnectsCount = new System.Windows.Forms.Label();
+            this.nConnectsCount = new System.Windows.Forms.NumericUpDown();
+            this.btnDrawGraph = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.nComputersCount)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nConnectsCount)).BeginInit();
             this.SuspendLayout();
+            // 
+            // gViewer
+            // 
+            this.gViewer.AsyncLayout = false;
+            this.gViewer.AutoScroll = true;
+            this.gViewer.BackwardEnabled = true;
+            this.gViewer.ForwardEnabled = true;
+            this.gViewer.Graph = null;
+            this.gViewer.Location = new System.Drawing.Point(12, 12);
+            this.gViewer.MouseHitDistance = 0.05D;
+            this.gViewer.Name = "gViewer";
+            this.gViewer.NavigationVisible = false;
+            this.gViewer.PanButtonPressed = false;
+            this.gViewer.SaveButtonVisible = true;
+            this.gViewer.Size = new System.Drawing.Size(542, 364);
+            this.gViewer.TabIndex = 0;
+            this.gViewer.ZoomF = 1D;
+            this.gViewer.ZoomFraction = 0.5D;
+            this.gViewer.ZoomWindowThreshold = 0.05D;
+            this.gViewer.SelectionChanged += new System.EventHandler(this.gViewer_SelectionChanged);
+            // 
+            // nComputersCount
+            // 
+            this.nComputersCount.Location = new System.Drawing.Point(707, 10);
+            this.nComputersCount.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nComputersCount.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nComputersCount.Name = "nComputersCount";
+            this.nComputersCount.Size = new System.Drawing.Size(40, 20);
+            this.nComputersCount.TabIndex = 1;
+            this.nComputersCount.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // lblComputersCount
+            // 
+            this.lblComputersCount.AutoSize = true;
+            this.lblComputersCount.Location = new System.Drawing.Point(606, 12);
+            this.lblComputersCount.Name = "lblComputersCount";
+            this.lblComputersCount.Size = new System.Drawing.Size(95, 13);
+            this.lblComputersCount.TabIndex = 2;
+            this.lblComputersCount.Text = "lblComputersCount";
+            // 
+            // lblConnectsCount
+            // 
+            this.lblConnectsCount.AutoSize = true;
+            this.lblConnectsCount.Location = new System.Drawing.Point(611, 38);
+            this.lblConnectsCount.Name = "lblConnectsCount";
+            this.lblConnectsCount.Size = new System.Drawing.Size(90, 13);
+            this.lblConnectsCount.TabIndex = 4;
+            this.lblConnectsCount.Text = "lblConnectsCount";
+            // 
+            // nConnectsCount
+            // 
+            this.nConnectsCount.Location = new System.Drawing.Point(707, 36);
+            this.nConnectsCount.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nConnectsCount.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nConnectsCount.Name = "nConnectsCount";
+            this.nConnectsCount.Size = new System.Drawing.Size(40, 20);
+            this.nConnectsCount.TabIndex = 3;
+            this.nConnectsCount.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // btnDrawGraph
+            // 
+            this.btnDrawGraph.Location = new System.Drawing.Point(573, 62);
+            this.btnDrawGraph.Name = "btnDrawGraph";
+            this.btnDrawGraph.Size = new System.Drawing.Size(174, 23);
+            this.btnDrawGraph.TabIndex = 5;
+            this.btnDrawGraph.Text = "btnDrawGraph";
+            this.btnDrawGraph.UseVisualStyleBackColor = true;
+            this.btnDrawGraph.Click += new System.EventHandler(this.btnDrawGraph_Click);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(759, 388);
+            this.Controls.Add(this.btnDrawGraph);
+            this.Controls.Add(this.lblConnectsCount);
+            this.Controls.Add(this.nConnectsCount);
+            this.Controls.Add(this.lblComputersCount);
+            this.Controls.Add(this.nComputersCount);
+            this.Controls.Add(this.gViewer);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.nComputersCount)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nConnectsCount)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown nComputersCount;
+        private System.Windows.Forms.Label lblComputersCount;
+        private System.Windows.Forms.Label lblConnectsCount;
+        private System.Windows.Forms.NumericUpDown nConnectsCount;
+        private System.Windows.Forms.Button btnDrawGraph;
+        private Microsoft.Glee.GraphViewerGdi.GViewer gViewer;
     }
 }
 
--- ADSCourseProject/ADSCourseProject/MainForm.cs
+++ ADSCourseProject/ADSCourseProject/MainForm.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.Glee.Drawing;
 
 namespace ADSCourseProject
 {
@@ -14,6 +15,118 @@ namespace ADSCourseProject
         public MainForm()
         {
             InitializeComponent();
+
+            Graph g = new Graph("graph");
+            g.GraphAttr.NodeAttr.Padding = 3;
+
+            g.AddEdge("S35", "36");
+            g.AddEdge("S35", "43");
+            g.AddEdge("43", "43");
+            g.AddEdge("36", "43");
+
+            gViewer.Graph = g;
+        }
+
+        private void btnDrawGraph_Click(object sender, EventArgs e)
+        {
+            int cc = (int) nComputersCount.Value;
+            int connections = cc*(int) nConnectsCount.Value;
+
+            int[,] peaks = new int[cc,cc];
+
+            for (int i = 0; i < cc; i++)
+                for (int j = 0; j < cc; j++)
+                    peaks[i, j] = -1;
+
+            Random r = new Random();
+            
+            int get = 0;
+            while (get <= connections)
+            {
+                int s = r.Next(0, cc);
+                int t = r.Next(0, cc);
+                int v = r.Next(0, 100);
+
+                if (s == t) continue;
+
+                if (peaks[s, t] == -1)
+                {
+                    int c = 0, z = 0;
+                    for (int i = 0; i < cc; i++)
+                    {
+                        if (peaks[s, i] != -1 || peaks[i, s] != -1) c++;
+                        if (peaks[t, i] != -1 || peaks[i, t] != -1) z++;
+                        ;
+                    }
+                    if (c > connections - 1 || z > connections - 1) continue;
+
+                    peaks[s, t] = v;
+                   // peaks[t, s] = v;
+                    get++;
+                }
+            }
+        
+
+
+            Graph g = new Graph("graph");
+            for (int i = 0; i < cc; i++)
+                for (int j = 0; j < cc; j++)
+                    if (peaks[i, j] != -1)
+                        g.AddEdge(i.ToString(), j.ToString());
+
+            gViewer.Graph = g;
+
+        }
+
+        object selectedObjectAttr;
+        object selectedObject;
+        void gViewer_SelectionChanged(object sender, EventArgs e)
+        {
+
+            if (selectedObject != null)
+            {
+                if (selectedObject is Edge)
+                    (selectedObject as Edge).Attr = selectedObjectAttr as EdgeAttr;
+                else if (selectedObject is Node)
+                    (selectedObject as Node).Attr = selectedObjectAttr as NodeAttr;
+
+                selectedObject = null;
+            }
+
+            if (gViewer.SelectedObject == null)
+            {
+               // label1.Text = "No object under the mouse";
+                //this.gViewer.SetToolTip(toolTip1, "");
+
+            }
+            else
+            {
+                selectedObject = gViewer.SelectedObject;
+
+                if (selectedObject is Edge)
+                {
+                    selectedObjectAttr = (gViewer.SelectedObject as Edge).Attr.Clone();
+                    (gViewer.SelectedObject as Edge).Attr.Color = Microsoft.Glee.Drawing.Color.Magenta;
+                    (gViewer.SelectedObject as Edge).Attr.Fontcolor = Microsoft.Glee.Drawing.Color.Magenta;
+                   // Edge edge = gViewer.SelectedObject as Edge;
+
+                    //here you can use e.Attr.Id or e.UserData to get back to you data
+                   //this.gViewer.SetToolTip(this.toolTip1, String.Format("edge from {0} {1}", edge.Source, edge.Target));
+
+                }
+                else if (selectedObject is Node)
+                {
+
+                    selectedObjectAttr = (gViewer.SelectedObject as Node).Attr.Clone();
+                    (selectedObject as Node).Attr.Fillcolor = Microsoft.Glee.Drawing.Color.Magenta;
+                    (selectedObject as Node).Attr.Color = Microsoft.Glee.Drawing.Color.Magenta;
+                    (selectedObject as Node).Attr.Fontcolor = Microsoft.Glee.Drawing.Color.Magenta;
+                    //here you can use e.Attr.Id to get back to your data
+                 //   this.gViewer.SetToolTip(toolTip1, String.Format("node {0}", (selectedObject as Node).Attr.Label));
+                }
+               // label1.Text = selectedObject.ToString();
+            }
+            gViewer.Invalidate();
         }
     }
 }
