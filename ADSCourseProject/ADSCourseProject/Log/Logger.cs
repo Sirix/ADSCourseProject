61fab5506a061659d2ba8977ab7bafc23932b322
Sirix <Sirix25@gmail.com>
2010-10-16 13:14:52 +0300

    Create logger (alpha), second attempt

---
+++ ADSCourseProject/ADSCourseProject/Log/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADSCourseProject.Log
+{
+    public class Logger
+    {
+        private static Logger _inst;
+        public static Logger instance
+        {
+            // ReSharper disable ConvertConditionalTernaryToNullCoalescing
+            get { return _inst == null ? (_inst = new Logger()) : _inst; }
+            // ReSharper restore ConvertConditionalTernaryToNullCoalescing
+        }
+
+        /// <summary>
+        /// Collection of log records, associated with their Log.Type
+        /// </summary>
+        public Dictionary<Type, string> Records { get; private set; }
+        protected Logger()
+        {
+            Records = new Dictionary<Type, string>();
+        }
+
+        
+
+    }
+}
---
+++ ADSCourseProject/ADSCourseProject/Log/Type.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADSCourseProject.Log
+{
+    /// <summary>
+    /// Specifies type of log entry
+    /// </summary>
+    [Flags]
+    public enum Type
+    {
+        /// <summary>
+        /// New data has been sended
+        /// </summary>
+        DataSended,
+        /// <summary>
+        /// Data has been received
+        /// </summary>
+        DataReceived,
+        /// <summary>
+        /// Data has been splitted
+        /// </summary>
+        DataSplitted,
+        /// <summary>
+        /// Data has been combined
+        /// </summary>
+        DataCombined,
+        /// <summary>
+        /// New packet has been sended
+        /// </summary>
+        PacketSended,
+        /// <summary>
+        /// Packet has been received
+        /// </summary>
+        PacketReceived,
+        /// <summary>
+        /// Packet has been moved
+        /// </summary>
+        PacketMoved
+    }
+}
