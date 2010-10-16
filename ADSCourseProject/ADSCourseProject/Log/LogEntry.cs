f831f6cd7cc0ef4ac3df9b621d22fe7689626aec
Sirix <Sirix25@gmail.com>
2010-10-16 19:41:42 +0300

    LogEntry.cs fix

---
+++ ADSCourseProject/ADSCourseProject/Log/LogEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ADSCourseProject
+{
+    internal class LogEntry
+    {
+        public Type LogType { get; set; }
+        public string Message { get; set; }
+    }
+}
